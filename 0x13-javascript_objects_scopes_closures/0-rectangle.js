8eadc7c459332e148d7aa04d10ff1c1af96c00d7
efeuwoghiren <efeuwoghiren@gmail.com>
2024-06-21 03:53:55 -0700

    Task 0. 0. Rectangle #0

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const Rectangle = require('./0-rectangle');
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+// empty class Rectangle that defines a rectangle
+class Rectangle {}
+module.exports = Rectangle;
